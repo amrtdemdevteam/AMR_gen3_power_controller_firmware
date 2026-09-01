6ceaa98310936b8b6b01bccf3b2ce2be03ab7820
thitipong-suh <thitipong.suhuttaporn@gmail.com>
2026-09-01 16:19:11 +0700

    Refactor main.cpp to enhance LED controller integration and update state callback functions

--- src/M7_app/main.cpp
+++ src/M7_app/main.cpp
@@ -4,6 +4,7 @@
 #include "ipc_status_checker.hpp"
 #include "state_callback.hpp"
 #include "button_event_monitor.hpp"
+#include "led_controller.hpp"
 
 PowerControlStateMachine powerControlHsm;
 
@@ -16,16 +17,27 @@ std::unique_ptr<IPCStatusChecker> ipcStatusChecker;
 
 ButtonEventMonitor::Config power_button_config = {
     .pin = 12,
-    .activeLow = false,
-    .debounceMs = 30,
-    .shortHoldMs = 3000,
-    .longHoldMs = 5000
+    .active_low = false,
+    .short_hold_ms = 3000,
+    .long_hold_ms = 5000
 };
 ButtonEventMonitor powerButtonMonitor(power_button_config);
 
+LedController::Config power_led_config = {
+    .pin = 13,
+    .active_low = false,
+    .blink_slow_hz = 1.0f,
+    .blink_fast_hz = 5.0f
+};
+LedController powerLedController(power_led_config);
+
 void setup() {
-    pinMode(13, OUTPUT);
-    digitalWrite(13, LOW);
+
+    powerLedController.begin();
+
+    //Register led controller to be used in the state callback functions
+    registerLedController(LedRole::POWER, &powerLedController);
+
     Serial.begin(115200);
 
     powerControlHsmRegisterCallbacks();
@@ -62,11 +74,11 @@ void loop() {
                     break;
                 case ButtonEventMonitor::Event::SHORT_HOLD:
                     Serial.println("Power button short hold");
-                    //powerControlHsm.postEvent(PowerControlStateMachine::Event::POWER_OFF);
+                    powerControlHsm.postEvent(PowerControlStateMachine::Event::POWER_ON);
                     break;
                 case ButtonEventMonitor::Event::LONG_HOLD:
                     Serial.println("Power button long hold");
-                    //powerControlHsm.postEvent(PowerControlStateMachine::Event::POWER_OFF);
+                    powerControlHsm.postEvent(PowerControlStateMachine::Event::POWER_OFF);
                     break;
                 default:
                     break;
@@ -77,12 +89,15 @@ void loop() {
             break;
     }
 
+    powerLedController.run();
+
     while (Serial.available() > 0) {
         const char cmd = static_cast<char>(Serial.read());
 
         switch (cmd) {
             case 'p': powerControlHsm.postEvent(PowerControlStateMachine::Event::POWER_ON); break;
             case 'o': powerControlHsm.postEvent(PowerControlStateMachine::Event::POWER_OFF); break;
+            case 'd': powerControlHsm.postEvent(PowerControlStateMachine::Event::INIT_DONE); break;
             case 'w': powerControlHsm.postEvent(PowerControlStateMachine::Event::FMS_WAKE_UP); break;
             case 'i': ipcStatusChecker->setIpcStatus(true); break;
             case 'x': ipcStatusChecker->setIpcStatus(false); break;
@@ -104,30 +119,32 @@ void loop() {
 }
 
 void powerControlHsmRegisterCallbacks() {
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SHUTDOWN, nullptr, on_enter_shutdown, on_exit_shutdown);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SERVICE_LAYER, nullptr, on_enter_service_layer, on_exit_service_layer);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SLEEP, nullptr, on_enter_sleep, on_exit_sleep);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::CHARGED, nullptr, on_enter_charged, on_exit_charged);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SERVICE_MODE, nullptr, on_enter_service_mode, on_exit_service_mode);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SERVICE_EMER, nullptr, on_enter_service_emer, on_exit_service_emer);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::CHARGING, nullptr, on_enter_charging, on_exit_charging);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::GO_TO_CHARGER, nullptr, on_enter_go_to_charger, on_exit_go_to_charger);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::WAIT_CHARGER, nullptr, on_enter_wait_charger, on_exit_wait_charger);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::CHARGING_IN_PROGRESS, nullptr, on_enter_charging_in_progress, on_exit_charging_in_progress);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::APPLICATION_LAYER, nullptr, on_enter_application_layer, on_exit_application_layer);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::OPERATION, nullptr, on_enter_operation, on_exit_operation);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::APP_EMER, nullptr, on_enter_app_emer, on_exit_app_emer);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::MANUAL, nullptr, on_enter_manual, on_exit_manual);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::STANDBY, nullptr, on_enter_standby, on_exit_standby);
-    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::AUTO, nullptr, on_enter_auto, on_exit_auto);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SHUTDOWN, nullptr, onEnterShutdown, onExitShutdown);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::INIT, nullptr, onEnterInit, onExitInit);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SERVICE_LAYER, nullptr, onEnterServiceLayer, onExitServiceLayer);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SLEEP, nullptr, onEnterSleep, onExitSleep);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::CHARGED, nullptr, onEnterCharged, onExitCharged);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SERVICE_MODE, nullptr, onEnterServiceMode, onExitServiceMode);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::SERVICE_EMER, nullptr, onEnterServiceEmer, onExitServiceEmer);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::CHARGING, nullptr, onEnterCharging, onExitCharging);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::GO_TO_CHARGER, nullptr, onEnterGoToCharger, onExitGoToCharger);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::WAIT_CHARGER, nullptr, onEnterWaitCharger, onExitWaitCharger);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::CHARGING_IN_PROGRESS, nullptr, onEnterChargingInProgress, onExitChargingInProgress);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::APPLICATION_LAYER, nullptr, onEnterApplicationLayer, onExitApplicationLayer);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::OPERATION, nullptr, onEnterOperation, onExitOperation);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::APP_EMER, nullptr, onEnterAppEmer, onExitAppEmer);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::MANUAL, nullptr, onEnterManual, onExitManual);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::STANDBY, nullptr, onEnterStandby, onExitStandby);
+    powerControlHsm.setStateCallbacks(PowerControlStateMachine::State::AUTO, nullptr, onEnterAuto, onExitAuto);
 
-    powerControlHsm.setTransitionFailedCallback(on_transition_failed);
+    powerControlHsm.setTransitionFailedCallback(onTransitionFailed);
 }
 
 void printHelp() {
     Serial.println("Commands:");
     Serial.println("  p: POWER_ON");
     Serial.println("  o: POWER_OFF");
+    Serial.println("  d: INIT_DONE");
     Serial.println("  w: FMS_WAKE_UP");
     Serial.println("  i: IPC_OK");
     Serial.println("  x: IPC_FAIL");
