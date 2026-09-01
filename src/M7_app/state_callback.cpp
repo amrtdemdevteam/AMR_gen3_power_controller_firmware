cac33dbd3ed9cb57d9d944d06675236090f553b3
thitipong-suh <thitipong.suhuttaporn@gmail.com>
2026-09-01 16:19:06 +0700

    Add LED controller management and refactor state transition functions

--- src/M7_app/state_callback.cpp
+++ src/M7_app/state_callback.cpp
@@ -1,5 +1,42 @@
 #include <Arduino.h>
+#include <array>
 #include "state_callback.hpp"
+#include "led_controller.hpp"
+
+constexpr size_t LED_ROLE_COUNT = static_cast<size_t>(LedRole::COUNT);
+std::array<LedController*, LED_ROLE_COUNT> g_led_controllers = {nullptr};
+
+bool isValidLedRole(LedRole led_role) {
+    return static_cast<size_t>(led_role) < LED_ROLE_COUNT;
+}
+
+LedController* getLedController(LedRole led_role) {
+    if (!isValidLedRole(led_role)) {
+        return nullptr;
+    }
+
+    LedController* led_controller = g_led_controllers[static_cast<size_t>(led_role)];
+    if (nullptr != led_controller) {
+        return led_controller;
+    }
+    return nullptr;
+}
+
+    
+
+bool registerLedController(LedRole led_role, LedController* led_controller) {
+    if (!isValidLedRole(led_role)) {
+        return false;
+    }
+
+    g_led_controllers[static_cast<size_t>(led_role)] = led_controller;
+    return true;
+}
+
+void clearLedControllers() {
+    g_led_controllers.fill(nullptr);
+}
+
 
 static void printVariantParams(const hsmcpp::VariantVector_t& params) {
     for (const auto& param : params) {
@@ -10,189 +47,233 @@ static void printVariantParams(const hsmcpp::VariantVector_t& params) {
     }
 }
 
-void on_state_changed(const hsmcpp::VariantVector_t& params) {
+void onStateChanged(const hsmcpp::VariantVector_t& params) {
     Serial.print("State changed -> ");
     printVariantParams(params);
 }
 
-bool on_enter_shutdown(const hsmcpp::VariantVector_t& params) {
+bool onEnterShutdown(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering SHUTDOWN");
     printVariantParams(params);
+
+    // Mock implementation for entering shutdown state
+    Serial.println("Shutdown IPC");
+    Serial.println("Shutdown motor driver");
+    Serial.println("Shutdown other peripherals");
+    Serial.println("Shutdown complete");
+
+    if (LedController* led = getLedController(LedRole::POWER)) {
+        led->setStateOff();
+    } // TODO: Handle LedController null
+
+    // Actually turn off final power gate here
     return true;
 }
 
-bool on_exit_shutdown() {
+bool onExitShutdown() {
     Serial.println("Exiting SHUTDOWN");
+
+    if (LedController* led = getLedController(LedRole::POWER)) {
+        led->setStateBlinkSlow();
+    } // TODO: Handle LedController null
+
+    
+
     return true;
 }
 
-bool on_enter_service_layer(const hsmcpp::VariantVector_t& params) {
+bool onEnterInit(const hsmcpp::VariantVector_t& params) {
+    Serial.println("Entering INIT");
+    printVariantParams(params);
+
+    Serial.println("Powering on");
+    Serial.println("Initializing subsystems");
+
+
+    return true;
+}
+
+bool onExitInit() {
+    Serial.println("Exiting INIT");
+
+    Serial.println("Subsystem initialization completed");
+    return true;
+}
+
+bool onEnterServiceLayer(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering SERVICE_LAYER");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_service_layer() {
+bool onExitServiceLayer() {
     Serial.println("Exiting SERVICE_LAYER");
     return true;
 }
 
-bool on_enter_sleep(const hsmcpp::VariantVector_t& params) {
+bool onEnterSleep(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering SLEEP");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_sleep() {
+bool onExitSleep() {
     Serial.println("Exiting SLEEP");
     return true;
 }
 
-bool on_enter_charged(const hsmcpp::VariantVector_t& params) {
+bool onEnterCharged(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering CHARGED");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_charged() {
+bool onExitCharged() {
     Serial.println("Exiting CHARGED");
     return true;
 }
 
-bool on_enter_service_mode(const hsmcpp::VariantVector_t& params) {
+bool onEnterServiceMode(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering SERVICE_MODE");
     printVariantParams(params);
+
+    if (LedController* led = getLedController(LedRole::POWER)) {
+        led->setStateSolid();
+    } // TODO: Handle LedController null
 
     return true;
 }
 
-bool on_exit_service_mode() {
+bool onExitServiceMode() {
     Serial.println("Exiting SERVICE_MODE");
     return true;
 }
 
-bool on_enter_service_emer(const hsmcpp::VariantVector_t& params) {
+bool onEnterServiceEmer(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering SERVICE_EMER");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_service_emer() {
+bool onExitServiceEmer() {
     Serial.println("Exiting SERVICE_EMER");
     return true;
 }
 
-bool on_enter_charging(const hsmcpp::VariantVector_t& params) {
+bool onEnterCharging(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering CHARGING");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_charging() {
+bool onExitCharging() {
     Serial.println("Exiting CHARGING");
     return true;
 }
 
-bool on_enter_go_to_charger(const hsmcpp::VariantVector_t& params) {
+bool onEnterGoToCharger(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering GO_TO_CHARGER");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_go_to_charger() {
+bool onExitGoToCharger() {
     Serial.println("Exiting GO_TO_CHARGER");
     return true;
 }
 
-bool on_enter_wait_charger(const hsmcpp::VariantVector_t& params) {
+bool onEnterWaitCharger(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering WAIT_CHARGER");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_wait_charger() {
+bool onExitWaitCharger() {
     Serial.println("Exiting WAIT_CHARGER");
     return true;
 }
 
-bool on_enter_charging_in_progress(const hsmcpp::VariantVector_t& params) {
+bool onEnterChargingInProgress(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering CHARGING_IN_PROGRESS");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_charging_in_progress() {
+bool onExitChargingInProgress() {
     Serial.println("Exiting CHARGING_IN_PROGRESS");
     return true;
 }
 
-bool on_enter_application_layer(const hsmcpp::VariantVector_t& params) {
+bool onEnterApplicationLayer(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering APPLICATION_LAYER");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_application_layer() {
+bool onExitApplicationLayer() {
     Serial.println("Exiting APPLICATION_LAYER");
     return true;
 }
 
-bool on_enter_operation(const hsmcpp::VariantVector_t& params) {
+bool onEnterOperation(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering OPERATION");
     printVariantParams(params);
+    if (LedController* led = getLedController(LedRole::POWER)) {
+        led->setState(LedController::State::SOLID);
+    }
     return true;
 }
 
-bool on_exit_operation() {
+bool onExitOperation() {
     Serial.println("Exiting OPERATION");
     return true;
 }
 
-bool on_enter_app_emer(const hsmcpp::VariantVector_t& params) {
+bool onEnterAppEmer(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering APP_EMER");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_app_emer() {
+bool onExitAppEmer() {
     Serial.println("Exiting APP_EMER");
     return true;
 }
 
-bool on_enter_manual(const hsmcpp::VariantVector_t& params) {
+bool onEnterManual(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering MANUAL");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_manual() {
+bool onExitManual() {
     Serial.println("Exiting MANUAL");
     return true;
 }
 
-bool on_enter_standby(const hsmcpp::VariantVector_t& params) {
+bool onEnterStandby(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering STANDBY");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_standby() {
+bool onExitStandby() {
     Serial.println("Exiting STANDBY");
     return true;
 }
 
-bool on_enter_auto(const hsmcpp::VariantVector_t& params) {
+bool onEnterAuto(const hsmcpp::VariantVector_t& params) {
     Serial.println("Entering AUTO");
     printVariantParams(params);
     return true;
 }
 
-bool on_exit_auto() {
+bool onExitAuto() {
     Serial.println("Exiting AUTO");
     return true;
 }
 
-void on_transition_failed(std::string message) {
+void onTransitionFailed(std::string message) {
     Serial.print("Transition failed: ");
     Serial.println(message.c_str());
 }
